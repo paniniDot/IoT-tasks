f5932414280486fe2bd26b2ebc7fafaec99757c8
Mattia Panni <pnmattia@gmail.com>
2022-11-22 17:21:11 +0100

    Update main

--- task2/task2.ino
+++ task2/task2.ino
@@ -1,8 +1,17 @@
+#include "Scheduler.h"
+
+#define SCHED_PERIOD 1000 //da aggiornare
+
+Scheduler* sched;
 
 void setup() {
-  
+    sched->init(SCHED_PERIOD);
+    
+    //Task* t0 = new ...
+    //t0.init(???)
+    //sched->addTask(t0);
 }
 
 void loop() {
-
+    sched->schedule();
 }
