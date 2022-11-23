cadd33e0f14b2903ee364037fb41b51c1166d611
Mattia Panni <mattia.panni@studio.unibo.it>
2022-11-23 23:00:06 +0100

    Sample task (not working lmao)

--- task2/task2.ino
+++ task2/task2.ino
@@ -1,15 +1,16 @@
 #include "Scheduler.h"
+#include "Sonar.h"
+#include "SonarTask.h"
 
-#define SCHED_PERIOD 1000 //da aggiornare
+#define SCHED_PERIOD 100 //da aggiornare
 
 Scheduler* sched;
 
 void setup() {
     sched->init(SCHED_PERIOD);
-    
-    //Task* t0 = new ...
-    //t0.init(???)
-    //sched->addTask(t0);
+    Task* t0 = new SonarTask(new Sonar(7, 8));
+    t0->init(SCHED_PERIOD);
+    sched->addTask(t0);
 }
 
 void loop() {
