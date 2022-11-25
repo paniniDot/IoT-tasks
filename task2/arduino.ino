520c4d3be79c26f2c2f7329e83b3cc82ad9a5d96
Mattia Panni <pnmattia@gmail.com>
2022-11-25 16:19:29 +0100

    Rename .ino file

--- task2/arduino.ino
+++ task2/arduino.ino
@@ -7,6 +7,8 @@
 #include "Pir.h"
 #include "Arduino.h"
 #include "ServoTimer2.h"
+#include "Button.h"
+#include <EnableInterrupt.h>
 
 #define SCHED_PERIOD 1000  //da aggiornare
 
@@ -14,6 +16,8 @@ Scheduler sched;
 
 void setup() {
   Serial.begin(9600);
+  Button* btn = new Button(2);
+  enableInterrupt(2, interruptCheck, RISING);
   sched.init(SCHED_PERIOD);
   ServoTimer2* servo = new ServoTimer2();
   servo->attach(6);
@@ -23,6 +27,16 @@ void setup() {
   t1->init(SCHED_PERIOD);
   sched.addTask(t0);
   sched.addTask(t1);
+}
+void interruptCheck() {
+  Serial.print("button = ");
+  Serial.println(manual);
+  if(manual == false){
+    manual = true;
+  } else{
+    manual = false;
+  }
+  delay(100);
 }
 
 void loop() {
