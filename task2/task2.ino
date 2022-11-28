2dc339875ae4ed92350a3676698293ac76545cc8
Mattia Panni <pnmattia@gmail.com>
2022-11-28 12:27:46 +0100

    Change generic task to specific one

--- task2/task2.ino
+++ task2/task2.ino
@@ -27,8 +27,8 @@ void setup()
   servo->attach(6);
   Task *t0 = new WaterTask(new Potentiometer(A0), servo, new Led(4), new Led(11));
   Task *t1 = new LightTask(new PhotoResistor(A1), new Pir(9), new Led(3));
-  Task *t2 = new SonarTask(new Sonar(7, 8));
-  Task *t3 = new ServoMotorTask(servo);
+  SonarTask *t2 = new SonarTask(new Sonar(7, 8));
+  ServoMotorTask *t3 = new ServoMotorTask(servo);
   t0->init(SCHED_PERIOD);
   t1->init(SCHED_PERIOD);
   t2->init(SCHED_PERIOD);
