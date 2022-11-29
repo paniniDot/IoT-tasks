5870158655a6ddce223da8baf71dbba39e380bd0
Mattia Panni <pnmattia@gmail.com>
2022-11-29 15:39:24 +0100

    Add source enumto event

--- task2/Event.h
+++ task2/Event.h
@@ -1,16 +1,23 @@
 #ifndef __EVENT__
 #define __EVENT__
 
+#include "EventSourceType.h"
 template <typename T>
 
 class Event {
 
     private:
+        EventSourceType srcType;
         T* eventArgs;
 
     public:
-        Event(T* eventArgs) {
+        Event(EventSourceType srctype, T* eventArgs) {
+            this->srcType = srctype;
             this->eventArgs = eventArgs;
+        }
+
+        EventSourceType getSrcType() {
+            return srcType;
         }
 
         T* getEventArgs() {
---
+++ task2/EventSourceType.h
@@ -0,0 +1,9 @@
+#ifndef __EVENTSOURCETYPE__
+#define __EVENTSOURCETYPE__
+
+enum EventSourceType {
+    SONAR,
+    SERVO
+};
+
+#endif
