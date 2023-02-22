19417393cf6caa1eb30f7fa2a41a51ffc9d1ab8d
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-02-22 14:27:21 +0100

    add emulated app without connection

--- task3/room-app/RemoteBlinkApp/app/src/main/java/com/example/remoteblinkapp/ControllerEmulatedActivity.java
+++ task3/room-app/RemoteBlinkApp/app/src/main/java/com/example/remoteblinkapp/ControllerEmulatedActivity.java
@@ -2,11 +2,17 @@ package com.example.remoteblinkapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -15,58 +21,68 @@ import java.nio.charset.StandardCharsets;
 
 public class ControllerEmulatedActivity extends AppCompatActivity {
 
+    private OutputStream bluetoothOutputStream;
     private Button remoteButton;
     private boolean ledState;
     private SeekBar seekBar;
     private int servoState;
-    private OutputStream emulatedBluetoothOutputStream;
-    private EmulatedClientConnectionThread connectionThread;
+
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controller);
+        ledState = false;
+        servoState = 0;
         initUI();
     }
 
     private void initUI() {
+        textView= findViewById(R.id.textView3);
         remoteButton = findViewById(R.id.remotebutton);
+        remoteButton.setEnabled(false);
         remoteButton.setOnClickListener((v) -> sendMessage());
         seekBar = findViewById(R.id.seekBar);
-        seekBar.setOnClickListener((v) -> sendMessage());
+        seekBar.setEnabled(false);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText("servo: "+String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                new Thread(() -> {
+                    servoState = seekBar.getProgress();
+                    runOnUiThread(() -> seekBar.setProgress(servoState));
+                }).start();
+            }
+        });
+        manageConnectedSocket();
     }
 
     private void sendMessage() {
         new Thread(() -> {
-            try {
-                String message = ledState ? "off\n" : "on\n";
-                emulatedBluetoothOutputStream.write(message.getBytes(StandardCharsets.UTF_8));
-                ledState = !ledState;
-                runOnUiThread(() -> remoteButton.setBackgroundColor(ledState ? Color.GREEN : Color.RED));
-                runOnUiThread(() -> seekBar.setProgress(servoState));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            String message = ledState ? "off\n" : "on\n";
+            remoteButton.setText("led: "+ (ledState ? "off" : "on"));
+            ledState = !ledState;
         }).start();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        connectionThread = new EmulatedClientConnectionThread(this::manageConnectedSocket);
-        connectionThread.start();
     }
 
-    private void manageConnectedSocket(Socket socket) {
-        try {
-            emulatedBluetoothOutputStream = socket.getOutputStream();
-            Log.i(C.TAG, "Connection successful!");
-        } catch (IOException e) {
-            Log.e(C.TAG, "Error occurred when creating output stream", e);
-        }
+    private void manageConnectedSocket() {
         runOnUiThread(() -> {
             remoteButton.setEnabled(true);
-            remoteButton.setBackgroundColor(Color.RED);
             seekBar.setEnabled(true);
             seekBar.setProgress(servoState);
         });
@@ -75,6 +91,5 @@ public class ControllerEmulatedActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        connectionThread.cancel();
     }
 }
