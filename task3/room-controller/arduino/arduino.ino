84b2ead9e9c34df95e97d90942abb3e9822bf241
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-02-22 11:06:23 +0100

    add string compare

--- task3/room-controller/arduino/arduino.ino
+++ task3/room-controller/arduino/arduino.ino
@@ -1,32 +1,32 @@
 #include "SoftwareSerial.h"
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+Servo myservo;            // create servo object to control a servo
 SoftwareSerial bt(2, 3);  // RX pin, TX pin
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial) {};
   bt.begin(9600);
-  Serial.println("sending cmd..");
-  bt.print("AT");
-  delay(1000);
-  bt.print("AT+NAMEisi00");  // Set the name to isiXX
-  delay(1000);
-
+  while (!Serial) {};
+  Serial.println("ready to go.");
   myservo.attach(6);
 }
 
 void loop() {
   if (bt.available()) {
-    Serial.write(bt.read());
-    myservo.write(0);
+    //Serial.write(bt.read());
+    String msg = bt.readStringUntil('\n');
+    Serial.println(msg);
+    if (msg == "180") {
+      Serial.println("1023");
+      myservo.write(1023);
+    } else if (msg == "0") {
+      Serial.println("0");
+      myservo.write(0);
+    }
   }
 
   if (Serial.available()) {
     bt.write(Serial.read());
-    myservo.write(1023);
   }
-  
-  delay(15);
 }
