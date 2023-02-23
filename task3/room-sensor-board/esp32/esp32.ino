c60fd38d8f5e3448a50600ffc432923467a35df0
Mattia Panni <mattia.panni@studio.unibo.it>
2023-02-23 22:45:33 +0100

    Update libraries

--- task3/room-sensor-board/esp32/esp32.ino
+++ task3/room-sensor-board/esp32/esp32.ino
@@ -1,9 +1,9 @@
 #include <WiFi.h>
-#include <PubSubClient.h>
+#include "PubSubClient.h"
 
 //define wifi
-const char* ssid = "nome_wifi";
-const char* password = "pass";
+const char* ssid = "iPhone di Mattia";
+const char* password = "qqqqqqqq";
 
 //forse la room service
 const char* mqtt_server = "broker";
@@ -13,14 +13,14 @@ const char* topic_light = "esp32/light";
 const char* topic_motion = "esp32/motion";
 
 //initialize this client
-PubSubClient publisher(new WiFiClient());
+WiFiClient client;
+PubSubClient publisher(client);
 
 void setup() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
   }
-
   publisher.setServer(mqtt_server, 1883);
 
   while (!publisher.connected()) {
