bf5c8ed4e9263c024469420c38fac54754c2975b
Alelover01 <76488337+Alelover01@users.noreply.github.com>
2023-02-24 17:17:32 +0100

    update js

--- task3/room-dashboard/home.js
+++ task3/room-dashboard/home.js
@@ -1,28 +1,50 @@
-//funzione che controlla se è abilitato il controllo manuale
-/*function enable() {
-    let manual = document.getElementById("chkManual").checked;
-    let btn_luce = document.getElementsByTagName("button");
-    let rg_tenda = document.getElementsByClassName("range");
-    if (manual == true) {
-        //il bottone e il range si devono abilitare
-        let a = btn_luce.removeAttr("disabled");
-        let b = rg_tenda.removeAttr("disabled");
-        console.log(a);
-        console.log(b);
-    } else {
-        //tutto disabilitato
-        let a = btn_luce.attr('disabled', 'disabled');
-        let b = rg_tenda.attr('disabled', 'disabled');
-        console.log(a);
-        console.log(b);
-    }
-}*/
-
 function change() {
     let rg_tenda = document.getElementById("rgTenda");
     let btn_Luce = document.getElementById("btnLuce");
     rg_tenda.classList.toggle("dark");
     btn_Luce.classList.toggle("dark");
     document.body.classList.toggle("dark");
-    // capire come cambiare lo stile alle altre cose
+    document.querySelector(".check").classList.toggle("dark");
+    document.querySelector(".luce").classList.toggle("dark");
+    document.querySelector(".tenda").classList.toggle("dark");
+    document.querySelector("aside").classList.toggle("dark");
+    let a = document.querySelectorAll("a");
+    a[0].classList.toggle("dark");
+    a[1].classList.toggle("dark");
+    let ul = document.querySelectorAll("ul > li");
+    ul[0].classList.toggle("dark");
+    ul[1].classList.toggle("dark");
+    if (btn_Luce.value == "ON") {
+        btn_Luce.value = "OFF";
+    } else {
+        btn_Luce.value = "ON";
+    }
 }
+
+
+window.addEventListener("load", function (event) {
+    let checkLuce = document.querySelector("#manualLuce");
+    let checkTenda = document.querySelector("#manualTenda");
+
+    checkLuce.addEventListener("change", function (event) {
+        let btn_Luce = document.getElementById("btnLuce");
+        if (this.checked) {
+            btn_Luce.removeAttribute("disabled", true);
+        } else {
+            btn_Luce.setAttribute("disabled", false);
+        }
+    });
+
+    checkTenda.addEventListener("change", function (event) {
+        let rg_tenda = document.getElementById("rgTenda");
+        if (this.checked) {
+            rg_tenda.removeAttribute("disabled", true);
+        } else {
+            rg_tenda.setAttribute("disabled", false);
+        }
+    });
+
+
+
+
+});
