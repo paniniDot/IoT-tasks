2ad04cfa49be9b0a47fcf0ea11579da07d0badd2
Eleonora Falconi <83820248+Eleonora-f@users.noreply.github.com>
2023-02-25 17:25:28 +0100

    Updated file

--- task3/room-controller/arduino/arduino.ino
+++ task3/room-controller/arduino/arduino.ino
@@ -1,9 +1,11 @@
 #include "SoftwareSerial.h"
 #include <Servo.h>
 #include <String.h>
+#define RX_PIN 2 // Define the RX pin for BT communication
+#define TX_PIN 3 // Define the TX pin for BT communication
 
 Servo myservo;            // create servo object to control a servo
-SoftwareSerial bt(2, 3);  // RX pin, TX pin
+SoftwareSerial bt(RX_PIN, TX_PIN); // RX pin, TX pin
 bool ledstatus = false;
 int servo = 0;
 
@@ -20,18 +22,20 @@ void loop() {
     //Serial.write(bt.read());
     String msg = bt.readStringUntil('\n');
     Serial.println(msg);
-    if (msg == "connesso") {
+    if (msh.startsWith("CONNESSO")) {
       bt.print("ledstatus");
       bt.println(ledstatus);
       delay(100);
       bt.print("servo");
       bt.println(servo);
-    } else if (msg == "on") {
-      Serial.println("accendo");
-      ledstatus = true;
-    } else if (msg == "off") {
-      Serial.println("spengo");
-      ledstatus = false;
+    } else if (msg.startsWith("LIGHT")) {
+        if (msg.endsWith("ON")) {
+          Serial.println("accendo");
+          ledstatus = true;
+        } else if (msg.endsWith("OFF")) {
+          Serial.println("spengo");
+          ledstatus = false;
+        }
     } else {
       myservo.write(map(msg.toInt(), 0, 100, 0, 1023));
       servo = msg.toInt();
