7a895fa2bf3d14a02024ef93ae4080080902ff08
riccardo fiorani <riccardofiorani@hotmail.it>
2023-02-28 16:56:28 +0100

    fix interface and observer

--- task3/room-controller/arduino/observer/arduino/arduino.ino
+++ task3/room-controller/arduino/observer/arduino/arduino.ino
@@ -3,15 +3,16 @@
 #include "src/Light.h"
 #include "src/sensor/Led.h"
 #include "src/Bluetooth.h"
-
-void setup()
-{
+SoftwareSerial *bt = new SoftwareSerial(2, 3);
+void setup() {
   Light *light = new Light(new Led(9));
-  SoftwareSerial *bt = new SoftwareSerial(2, 3);
+  Serial.begin(9600);
+  while (!Serial) {};
+  bt->begin(9600);
+  Serial.println("ready to go.");
   Bluetooth *bluetooth = new Bluetooth(bt);
   light->attach(bluetooth);
 }
 
-void loop()
-{
+void loop() {
 }
