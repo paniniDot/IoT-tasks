6af1bc72a0bc3c8a9b04006811e3821d3ca15b05
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-03-08 09:39:31 +0100

    fix charts

--- task3/room-dashboard/js/dashboard.js
+++ task3/room-dashboard/js/dashboard.js
@@ -3,20 +3,52 @@ const ctx = document.getElementById('myChart');
 new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
+    labels: ['1', '2', '3', '4', '5', '6'],
+    datasets: [
+      {
+        label: 'status',
+        data: [0, 1, 0, 1],
+        fill: false,
+        stepped: true,
+      }
+    ]
   },
   options: {
+    responsive: true,
+    plugins: {
+      tooltip: {
+        mode: 'index',
+        intersect: false
+      },
+      title: {
+        display: true,
+        text: 'light'
+      }
+    },
+    hover: {
+      mode: 'index',
+      intersec: false
+    },
     scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'hour'
+        }
+      },
       y: {
-        beginAtZero: true
+        title: {
+          display: true,
+          text: 'light'
+        },
+        min: 0,
+        max: 1,
+        ticks: {
+          stepSize: 1
+        }
       }
     }
-  }
+  },
 });
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -25,7 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
   range.addEventListener('input', (event) => {
     const value = event.target.value;
     valueSpan.textContent = `${value}`;
-    const offset = ((value - range.min +2.5) / (range.max - range.min +4.5)) * range.offsetWidth;
+    const offset = ((value - range.min + 2.5) / (range.max - range.min + 4.5)) * range.offsetWidth;
     valueSpan.style.transform = `translateX(${offset}px) translatey(-120%)`;
   });
 });
