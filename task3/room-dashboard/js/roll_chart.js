f8a7c94162e5eec16e0be0a2a626a78eb9f804bb
Mattia Panni <mattia.panni@studio.unibo.it>
2023-06-14 13:22:53 +0200

    Update graphic

--- task3/room-dashboard/js/roll_chart.js
+++ task3/room-dashboard/js/roll_chart.js
@@ -2,27 +2,37 @@ const data = [
   {
     x: [],
     y: [],
-    mode: 'lines',
+    mode: 'lines+markers',
     line: {
-      color: 'blue'
+      color: 'red',
+      shape: 'linear'
     }
   }
 ];
 
 const layout = {
+  title: 'Storico tenda',
   xaxis: {
     type: 'category',
-    title: 'Storico tenda',
-    tickformat: '%Y-%m-%d %H:%M'
+    tickformat: '%Y-%m-%d %H:%M:%S'
   },
   yaxis: {
     title: '%'
   }
 };
 
+let counter = 0;
+
 function updateRollChart(time, value) {
   data[0].x.push(time);
   data[0].y.push(value);
+
+  // Ridimensiona il grafico se supera 10 valori
+  if (data[0].x.length > 15) {
+    data[0].x.shift();
+    data[0].y.shift();
+  }
+
   Plotly.update('rollchart', data, layout);
 }
 
@@ -34,6 +44,10 @@ setInterval(function () {
     // Dati ricevuti dal server
     const data = response.data;
     console.log(data);
+    const time = convertTimestampToFormattedDate(data.timestamp);
+    const measure = data.measure;
+    const name = data.name;
+    updateRollChart(time, measure);
   })
   .catch(error => {
     // Gestire eventuali errori
@@ -42,13 +56,14 @@ setInterval(function () {
 }, 1000);
 
 function convertTimestampToFormattedDate(timestamp) {
-  const date = new Date(timestamp * 1000); // Moltiplica per 1000 per convertire da millisecondi a secondi
+  const date = new Date(timestamp); // Moltiplica per 1000 per convertire da millisecondi a secondi
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0'); // Aggiunge uno zero iniziale se necessario
   const day = String(date.getDate()).padStart(2, '0');
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
   
-  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}`;
+  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
   return formattedDate;
 }
