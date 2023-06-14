445a7ce783d6f8eec9eaa0e0e0ee4e86c7346d95
Mattia Panni <mattia.panni@studio.unibo.it>
2023-06-14 13:36:58 +0200

    Update everything

--- task3/room-dashboard/js/dashboard.js
+++ task3/room-dashboard/js/dashboard.js
@@ -1,61 +1,6 @@
-const ctx = document.getElementById('lightchart');
-
-new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ['1', '2', '3', '4', '5', '6'],
-    datasets: [
-      {
-        label: 'status',
-        data: ['ON', 'ON', 'OFF', 'ON', 'OFF', 'OFF', 'ON'],
-        fill: false,
-        stepped: true,
-      }
-    ]
-  },
-  options: {
-    responsive: true,
-    plugins: {
-      tooltip: {
-        mode: 'index',
-        intersect: false
-      },
-    },
-    hover: {
-      mode: 'index',
-      intersec: false
-    },
-    scales: {
-      x: {
-        title: {
-          display: true,
-          text: 'hour'
-        }
-      },
-      y: {
-        type: 'category',
-        labels: ['ON', 'OFF'],
-      }
-    }
-  },
-});
-
-
-
-
 
 document.addEventListener('DOMContentLoaded', () => {
-  const checkbox = document.getElementById("flexCheckDefault");
   const lightswitch = document.getElementById("lightswitch");
-
-  checkbox.addEventListener("click", () => {
-    if (!checkbox.checked) {
-      lightswitch.disabled = true;
-    } else {
-      lightswitch.disabled = false;
-    }
-  });
-
   const lightbulbIcon = document.querySelector(".bi-lightbulb-off");
 
   lightswitch.addEventListener("click", () => {
@@ -72,6 +17,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const value = event.target.value;
     valueSpan.textContent = `${value}`;
     const offset = ((value - range.min + 2) / (range.max - range.min + 4)) * range.offsetWidth;
-    valueSpan.style.transform = `translateX(${offset}px) translatey(-120%)`;
+    valueSpan.style.transform = `translateX(${offset}px) translateY(-120%)`;
   });
 });
