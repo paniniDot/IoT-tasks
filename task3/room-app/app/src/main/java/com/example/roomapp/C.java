17532eeda0d6f69466e4882289b45b613afa5d0a
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-07-04 17:29:57 +0200

    rename

--- task3/room-app/app/src/main/java/com/example/roomapp/C.java
+++ task3/room-app/app/src/main/java/com/example/roomapp/C.java
@@ -3,7 +3,7 @@ package com.example.roomapp;
 import java.util.UUID;
 
 public class C {
-    public static final String TAG = "RoomApp";
+    public static final String TAG = "roomapp";
     public static final UUID DEFAULT_DEVICE_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     public class emulator {
