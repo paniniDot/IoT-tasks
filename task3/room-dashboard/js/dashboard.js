f935087e38e7eee36962459bdfd860b129b0db8b
Mattia Panni <mattia.panni@studio.unibo.it>
2023-07-04 17:00:44 +0200

    Update js

--- task3/room-dashboard/js/dashboard.js
+++ task3/room-dashboard/js/dashboard.js
@@ -1,3 +1,10 @@
+function createJson(obj, value) {
+  const json = {
+    name: obj,
+    measure: value,
+  }
+  return JSON.stringify(json);
+}
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -8,65 +15,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   lightButton.addEventListener("click", () => {
-    let manual_light = false;
-    if (lightButton.checked) {
-      manual_light = true;
-      lightswitch.addEventListener("click", () => {
-        if (manual_light == true) {
-          const value = lightswitch.checked ? 1 : 0;
-          const jsonData = createJson("light", value);
-          axios.post('http://localhost:8080/api/data', jsonData)
-            .then(response => {
-              console.log("messaggio inviato");
-            })
-            .catch(error => {
-              console.error(error);
-            });
-          lightbulbIcon.classList.replace(
-            lightswitch.checked ? "bi-lightbulb-off" : "bi-lightbulb",
-            lightswitch.checked ? "bi-lightbulb" : "bi-lightbulb-off"
-          );
-        }
-      });
-    }
-    const value = manual_light ? 1 : 0;
-    const jsonData = createJson("manual_light", value);
-    axios.post('http://localhost:8080/api/data', jsonData)
-      .then(response => {
-        console.log("messaggio inviato");
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    lightswitch.addEventListener("click", () => {
+      if (lightButton.checked) {
+        axios.post('http://localhost:8080/api/data', createJson("light", lightswitch.checked ? 1 : 0))
+        lightbulbIcon.classList.replace(
+          lightswitch.checked ? "bi-lightbulb-off" : "bi-lightbulb",
+          lightswitch.checked ? "bi-lightbulb" : "bi-lightbulb-off"
+        );
+      }
+    });
+    axios.post('http://localhost:8080/api/data', createJson("manual_light", lightButton.checked ? 1 : 0))
   });
 
-  function createJson(obj, value) {
-    const json = {
-      name: obj,
-      measure: value,
-    }
-    return JSON.stringify(json);
-  }
-
   rollButton.addEventListener("click", () => {
-    if (rollButton.checked) {
-      const range = document.getElementById('rollrange');
-      const valueSpan = document.getElementById('rollvalue');
-      range.addEventListener('input', (event) => {
+    const range = document.getElementById('rollrange');
+    const valueSpan = document.getElementById('rollvalue');
+    range.addEventListener('input', (event) => {
+      if(rollButton.checked) {
         const value = event.target.value;
-
         axios.post('http://localhost:8080/api/data', createJson("roll", value))
-          .then(response => {
-            log("messaggio inviato");
-          })
-          .catch(error => {
-            console.error(error);
-          });
-
         valueSpan.textContent = `${value}`;
         const offset = ((value - range.min + 2) / (range.max - range.min + 4)) * range.offsetWidth;
         valueSpan.style.transform = `translateX(${offset}px) translateY(-120%)`;
-      });
-    }
+      }
+    });
+    axios.post('http://localhost:8080/api/data', createJson("manual_roll", rollButton.checked ? 1 : 0))
   });
 });
