9c8acd7114c24cd38fba78b9d55abeb4977581f1
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-07-05 23:54:09 +0200

    fix js and add new features

--- task3/room-dashboard/js/dashboard.js
+++ task3/room-dashboard/js/dashboard.js
@@ -8,9 +8,11 @@ function createJson(obj, value) {
 const rollButton = document.getElementById("rollcheck");
 const lightswitch = document.getElementById("lightswitch");
 const lightButton = document.getElementById("lightcheck");
-const lightbulbIcon = document.querySelector(".bi-lightbulb-off");
+const lightbulbIcon = document.getElementById("icon");
 const range = document.getElementById('rollrange');
 const valueSpan = document.getElementById('rollvalue');
+lightswitch.disabled = true;
+range.disabled = true;
 
 function updateDashboard(name, value) {
   if (name == "light") {
@@ -25,27 +27,34 @@ function updateDashboard(name, value) {
 
 document.addEventListener('DOMContentLoaded', () => {
   lightButton.addEventListener("click", () => {
+    if (lightButton.checked) {
+      lightswitch.disabled = false;
+    } else {
+      lightswitch.disabled = true;
+    }
     lightswitch.addEventListener("click", () => {
-      if (lightButton.checked) {
-        sendMessage(createJson("light", lightswitch.checked ? 1 : 0));
-        lightbulbIcon.classList.replace(
-          lightswitch.checked ? "bi-lightbulb-off" : "bi-lightbulb",
-          lightswitch.checked ? "bi-lightbulb" : "bi-lightbulb-off"
-        );
+      sendMessage(createJson("light", lightswitch.checked ? 1 : 0));
+      if (lightswitch.checked) {
+        lightbulbIcon.classList.replace("bi-lightbulb-off", "bi-lightbulb");
+      } else {
+        lightbulbIcon.classList.replace("bi-lightbulb", "bi-lightbulb-off");
       }
     });
     sendMessage(createJson("manual_light", lightButton.checked ? 1 : 0));
   });
 
   rollButton.addEventListener("click", () => {
+    if (rollButton.checked) {
+      range.disabled = false;
+    } else {
+      range.disabled = true;
+    }
     range.addEventListener('input', (event) => {
-      if (rollButton.checked) {
-        const value = event.target.value;
-        sendMessage(createJson("roll", value));
-        valueSpan.textContent = `${value}`;
-        const offset = ((value - range.min + 2) / (range.max - range.min + 4)) * range.offsetWidth;
-        valueSpan.style.transform = `translateX(${offset}px) translateY(-120%)`;
-      }
+      const value = event.target.value;
+      sendMessage(createJson("roll", value));
+      valueSpan.textContent = `${value}`;
+      const offset = ((value - range.min + 2) / (range.max - range.min + 4)) * range.offsetWidth;
+      valueSpan.style.transform = `translateX(${offset}px) translateY(-120%)`;
     });
     sendMessage(createJson("manual_roll", rollButton.checked ? 1 : 0));
   });
